65e1d275c21bc8aa4b8f0a4d976e3d22751be4ab
kilu1312as <60524047+kilu1312as@users.noreply.github.com>
2020-08-23 18:31:22 +0300

    2

---
+++ test_gui.py
@@ -0,0 +1,11 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
